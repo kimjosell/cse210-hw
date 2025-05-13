b29ca98accfe163f2b90ccd4c1bef1fa8a6f7206
Kim Josell <145394112+kimjosell@users.noreply.github.com>
2025-05-13 11:48:19 -0600

    Working on the assignements ofg the week 2 resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Nintendo";
+        job1._startYear = "2020";
+        job1._endYear = "2023";
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Data Analyst";
+        job2._company = "Apple";
+        job2._startYear = "2020";
+        job2._endYear = "2023";
+
+        Resume resume1 = new Resume();
+        resume1._name = "Kim Josell";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
     }
 }
